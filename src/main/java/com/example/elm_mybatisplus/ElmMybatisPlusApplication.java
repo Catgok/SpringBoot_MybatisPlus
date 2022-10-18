d07238e1434fd2e30472de52d2f543fb7bace6b6
catgok <catgok@163.com>
2022-10-18 23:54:21 +0800

    SpringBoot MybatisPlus example init

---
+++ src/main/java/com/example/elm_mybatisplus/ElmMybatisPlusApplication.java
@@ -0,0 +1,13 @@
+package com.example.elm_mybatisplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElmMybatisPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElmMybatisPlusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/elm_mybatisplus/ElmMybatisPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.elm_mybatisplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElmMybatisPlusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
